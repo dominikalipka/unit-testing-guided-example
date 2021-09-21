d0d458673227b3f7ab37f300b4512d03d589b8c2
Dominika Lipka <dominika.lipka96@gmail.com>
2021-09-21 17:16:47 +0200

    sum and subtract pass all the tests, one test left for division

--- calculator.js
+++ calculator.js
@@ -1,17 +1,36 @@
 function sum(a, b) {
-  return;
+  if (!a && !b) {
+    return 0;
+  } else if (!a) {
+    return b + 0;
+  } else if (!b) {
+    return a + 0
+  } else
+  return a + b;
 }
 
 function subtract(a, b) {
-  return;
+  if (!a && !b) {
+    return 0;
+  } else if (!a) {
+    return b - 0;
+  } else if (!b) {
+    return a - 0;
+  } else {
+  return a - b;
+  }
 }
 
 function divide(a, b) {
-  return;
-}
+  return a / b;
+  }
 
 function multiply(a, b) {
+  if (b === 0) {
+
+  } else {
   return a * b;
+  }
 }
 
 // The following is required to make unit tests work. Please ignore it.
